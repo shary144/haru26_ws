72d2a44555888151891228ca57ec6fefaf6e9ea9
Southern12014 <12014minami@gmail.com>
2026-03-05 09:37:51 +0900

    package+launch名の登録だけで起動できるlaunchです

--- src/self_driving/launch/PrepareAll.launch.py
+++ src/self_driving/launch/PrepareAll.launch.py
@@ -1,24 +1,29 @@
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch_ros.actions import Node
-import os
+from launch.launch_description_sources import AnyLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
+import os
 
 def generate_launch_description():
 
-    keywords= [("lidar_imu","lidar_imu_bringup.launch.py"),
-               ("odometry_plugins","odometry_bridge_launch.xml"),
-               ("urg_node2","urg_node2.launch.py")]
-    
+    keywords = [
+        ("lidar_imu", "lidar_imu_bringup.launch.py"),
+        ("odometry_plugins", "odometry_bridge_launch.xml")
+    ]
+
     launch_path_series = []
-    for (pkg, launch_file) in keywords:
+
+    for pkg, launch_file in keywords:
         pkg_dir = get_package_share_directory(pkg)
-        launch_path = os.path.join(pkg_dir, 'launch', launch_file)
+        launch_path = os.path.join(pkg_dir, "launch", launch_file)
+
         if not os.path.exists(launch_path):
-            raise FileNotFoundError(f"Launch file '{launch_file}' not found in package '{pkg}' at path: {launch_path}")
-        launch_path_series.append(IncludeLaunchDescription(PythonLaunchDescriptionSource(launch_path)))
-        
-    pkg_dir = get_package_share_directory('self_driving')
+            raise FileNotFoundError(f"{launch_path} が見つかりません")
+
+        launch_path_series.append(
+            IncludeLaunchDescription(
+                AnyLaunchDescriptionSource(launch_path)
+            )
+        )
 
     return LaunchDescription(launch_path_series)
